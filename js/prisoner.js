0c8b4d9656e10d2d3fabed1a1f4d438369532194
Aneta <anetabruzda@student.agh.edu.pl>
2023-10-22 23:03:49 +0200

    working on prisoner panel

---
+++ js/prisoner.js
@@ -0,0 +1,57 @@
+function load_data(query)
+{
+	if(query.length > 2)
+	{
+		var form_data = new FormData();
+
+		form_data.append('query', query);
+
+		var ajax_request = new XMLHttpRequest();
+
+		ajax_request.open('POST', 'process_data.php');
+
+		ajax_request.send(form_data);
+
+		ajax_request.onreadystatechange = function()
+		{
+			if(ajax_request.readyState == 4 && ajax_request.status == 200)
+			{
+				var response = JSON.parse(ajax_request.responseText);
+
+				var html = '<div class="list-group">';
+
+				if(response.length > 0)
+				{
+					for(var count = 0; count < response.length; count++)
+					{
+						html += '<a href="#" class="list-group-item list-group-item-action">'+response[count].name+ ' ' +response[count].surname+ ', ' +response[count].prisoner_id+ '</a>';
+					}
+				}
+				else
+				{
+					html += '<a href="#" class="list-group-item list-group-item-action disabled">No Data Found</a>';
+				}
+
+				html += '</div>';
+
+				document.getElementById('search_result').innerHTML = html;
+			}
+		}
+	}
+	else
+	{
+		document.getElementById('search_result').innerHTML = '';
+	}
+}
+
+
+
+const showButton = document.querySelector("#table-btn")
+const table = document.querySelector(".table");
+
+function openTable() {
+
+	table.classList.toggle("d-none");
+}
+
+showButton.addEventListener("click", openTable);
--- prisoner_panel.php
+++ prisoner_panel.php
@@ -7,14 +7,6 @@ if ((!isset($_SESSION['zalogowany'])) && ($_SESSION['zalogowany']!==1))
     header('Location: logpage.php');
     exit();
 }
-
-//$_SESSION['name'] = $name;
-//$query = "SELECT * FROM Users WHERE username = $1";
-//$name_query = "SELECT name FROM public.\"Users\" WHERE \"id\"='$login' AND \"password\"='$password'";
-//$name = $pdo->query($name_query);
-//<form action="prisoner.php" method="post">
-//<input type="text" name="prisoner_id" class="id"  placeholder="prisoner_id" id="prisoner_id"> 
-//</form>
 ?>
 
 <!DOCTYPE html>
@@ -24,7 +16,7 @@ if ((!isset($_SESSION['zalogowany'])) && ($_SESSION['zalogowany']!==1))
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web prison management system - Home page</title>
-    <link rel="stylesheet" href="./style/homepage.css">
+    <link rel="stylesheet" href="./style/prisoner.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/a6f2b46177.js" crossorigin="anonymous"></script>
@@ -52,72 +44,46 @@ if ((!isset($_SESSION['zalogowany'])) && ($_SESSION['zalogowany']!==1))
 
     <header>
     
-    <div class="dropdown">
-        <input type="text" name="search_box" class="form-control form-control-lg" placeholder="Wpisz imię i nazwisko szukanego więźnia" onkeyup="javascript:load_data(this.value)" />
-		<span id="search_result"></span>
-    </div>
+        <div class="dropdown">
+            <input type="text" name="search_box" class="form-control form-control-lg" placeholder="Wpisz imię i nazwisko szukanego więźnia" onkeyup="javascript:load_data(this.value)" />
+		    <span id="search_result"></span>
+        </div>
 
-    <?php
-        
-        if (isset($_SESSION['prisoner_id'])) {
-            echo "Imie: " . $_SESSION['name_prisoner'];
-            echo "Nazwisko: " . $_SESSION['surname_prisoner'];
-        }
-    ?>
+        <button id="table-btn"class="btn-add bg-dark text-light" onclick="openTable()">Wyświetl wszytsko</button>
 
-        
+        <div class="table">
+        <table class="my-table">
+            <tr>
+                <th>ID</th>
+                <th>Imię</th>
+                <th>Nazwisko</th>
+                <th>Płeć</th>
+                <th>Data urodzenia</th>
+            </tr>
+            <?php
+
+                include 'select_all.php';
+
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row['prisoner_id'] . "</td>";
+                echo "<td>" . $row['name'] . "</td>";
+                echo "<td>" . $row['surname'] . "</td>";
+                echo "<td>" . $row['sex'] . "</td>";
+                echo "<td>" . $row['birth_date'] . "</td>";
+                echo "</tr>";
+            }
+            ?>
+        </table>
+        </div>
+       
+
     </header>
-
+    <script src="./js/prisoner.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
     </script>
-    <script>
-        function load_data(query)
-{
-	if(query.length > 2)
-	{
-		var form_data = new FormData();
-
-		form_data.append('query', query);
-
-		var ajax_request = new XMLHttpRequest();
-
-		ajax_request.open('POST', 'process_data.php');
-
-		ajax_request.send(form_data);
-
-		ajax_request.onreadystatechange = function()
-		{
-			if(ajax_request.readyState == 4 && ajax_request.status == 200)
-			{
-				var response = JSON.parse(ajax_request.responseText);
-
-				var html = '<div class="list-group">';
-
-				if(response.length > 0)
-				{
-					for(var count = 0; count < response.length; count++)
-					{
-						html += '<a href="#" class="list-group-item list-group-item-action">'+response[count].name+ ' ' +response[count].surname+ ', ' +response[count].prisoner_id+ '</a>';
-					}
-				}
-				else
-				{
-					html += '<a href="#" class="list-group-item list-group-item-action disabled">No Data Found</a>';
-				}
-
-				html += '</div>';
-
-				document.getElementById('search_result').innerHTML = html;
-			}
-		}
-	}
-	else
-	{
-		document.getElementById('search_result').innerHTML = '';
-	}
-}
-</script>
+    
 </body>
 
 </html> 
