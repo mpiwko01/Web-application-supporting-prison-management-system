f2556df93228045e36f1059ba86753f61d37a3af
Aneta <anetabruzda@student.agh.edu.pl>
2023-10-27 20:35:39 +0200

    update move prisoner

--- move_prisoner.php
+++ move_prisoner.php
@@ -18,14 +18,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             $dbconn = mysqli_connect("mysql.agh.edu.pl:3306", "anetabru", "Aneta30112001", "anetabru");
 
-            $query_update = "UPDATE `cell_history` SET `to_date`='$selectedDate' WHERE `prisoner_id`='$prisoner_id' AND `to_date` IS NULL";
+            $query_cell = "SELECT * FROM cell_history WHERE `prisoner_id`='$prisoner_id' AND `to_date` IS NULL";
 
-            $query = "INSERT INTO `cell_history` VALUES ('$prisoner_id', '$selectedCell', '$selectedDate', NULL)";
+            $result_cell = mysqli_query($dbconn, $query_cell);
 
-            $result_update = mysqli_query($dbconn, $query_update);
-            $result = mysqli_query($dbconn, $query);
+            if($result_cell) {
+                $row = mysqli_fetch_array($result_cell);
+                if ($row) {
+                    $currentCell =  $row['cell_nr'];
+                    if ($currentCell != $selectedCell) {
+                        $query_update = "UPDATE `cell_history` SET `to_date`='$selectedDate' WHERE `prisoner_id`='$prisoner_id' AND `to_date` IS NULL";
+
+                        $query = "INSERT INTO `cell_history` VALUES ('$prisoner_id', '$selectedCell', '$selectedDate', NULL)";
+
+                        $result_update = mysqli_query($dbconn, $query_update);
+
+                        $result = mysqli_query($dbconn, $query);
                 
-            echo "success";
+                        echo "success";
+                    }
+                    else {
+                        //nie dodaj do bazy
+                    }
+
+                }
+            }
+
+            
         }
     }
     ?>
