ff5d0cd18493c57766a801582dd3e390668ba623
SebastianGarncarek <sgarncarek@student.agh.edu.pl>
2023-11-14 10:19:06 +0100

    FIXED ALL OUR PROBLEMS BY PIWKO
    Calendar (finally?) works perfectly and smoothly

--- calendar/calendar.php
+++ calendar/calendar.php
@@ -19,8 +19,6 @@ session_start();
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid@4.3.0/main.min.css">
     <link rel="stylesheet" href="calendar.css">
 
-    <script src="calendar.js"></script>
-
     <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/5.7.0/locale/pl.js"></script>-->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
