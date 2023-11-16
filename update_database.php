4d51c4b270a245a0ba1e590497f1aa16331e950e
Aneta <anetabruzda@student.agh.edu.pl>
2023-11-16 01:22:56 +0100

    automatic prisoner removal improved

--- update_database.php
+++ update_database.php
@@ -13,16 +13,20 @@ WHERE ps.to_date = '$curDate'";
 
 $result_prisoners = mysqli_query($dbconn, $query_prisoners);
 
-if ($result_prisoners && $result_prisoners->num_rows > 0) {
+if ($result_prisoners) {
 
     $query_cell_history = "UPDATE `cell_history` AS c
     INNER JOIN `prisoner_sentence` AS ps ON c.prisoner_id = ps.prisoner_id
     SET c.to_date = '$curDate'
-    WHERE c.to_date IS NULL";
+    WHERE ps.prisoner_id IN (
+        SELECT prisoner_id 
+        FROM prisoner_sentence 
+        WHERE `to_date` = '$curDate' AND release_date IS NULL
+    ) AND c.to_date IS NULL";
 
     $result_cell_history = mysqli_query($dbconn, $query_cell_history);
 
-    $query_prisoner_sentence = "UPDATE `prisoner_sentence` SET `release_date` = '$curDate' WHERE `release_date` IS NULL";
+    $query_prisoner_sentence = "UPDATE `prisoner_sentence` SET `release_date` = '$curDate' WHERE `to_date` = '$curDate' AND `release_date` IS NULL";
 
     $result_prisoner_sentence = mysqli_query($dbconn, $query_prisoner_sentence);
     
