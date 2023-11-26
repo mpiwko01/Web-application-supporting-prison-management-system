1188ebbb61e4e3f254dad4c45d05d0ddcc8496cc
SebastianGarncarek <sgarncarek@student.agh.edu.pl>
2023-11-26 23:48:34 +0100

    Sort prisoners
    Added sorting prisoners in order of surname and name

--- select_all.php
+++ select_all.php
@@ -1,5 +1,5 @@
 <?php
 $dbconn = mysqli_connect("mysql.agh.edu.pl:3306", "anetabru", "Aneta30112001", "anetabru");
-$query = "SELECT * FROM  `prisoners`";
+$query = "SELECT * FROM  `prisoners` ORDER BY `surname` ASC, `name` ASC";
 $result = mysqli_query($dbconn,$query);
 ?>
