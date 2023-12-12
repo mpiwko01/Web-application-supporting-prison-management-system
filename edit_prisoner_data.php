d798bf293c69b2bdbcb12cecf6545a8e20d8db1a
mpiwko01 <mpiwko@student.agh.edu.pl>
2023-12-12 14:44:10 +0100

    fixed update pesel

--- edit_prisoner_data.php
+++ edit_prisoner_data.php
@@ -3,13 +3,14 @@ session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    if (isset($_POST['prisonerId']) && !empty($_POST['prisonerId']) && isset($_POST['name']) && !empty($_POST['name']) && isset($_POST['surname']) && !empty($_POST['surname']) && isset($_POST['sex']) && !empty($_POST['sex']) && isset($_POST['birthDate']) && !empty($_POST['birthDate']) && isset($_POST['street']) && !empty($_POST['street'])&& isset($_POST['houseNumber']) && !empty($_POST['houseNumber'])&& isset($_POST['city']) && !empty($_POST['city']) && isset($_POST['zipCode']) && !empty($_POST['zipCode']) && isset($_POST['startDate']) && !empty($_POST['startDate']) && isset($_POST['endDate']) && !empty($_POST['endDate']) && isset($_POST['crime']) && !empty($_POST['crime'])) {
+    if (isset($_POST['prisonerId']) && !empty($_POST['prisonerId']) && isset($_POST['name']) && !empty($_POST['name']) && isset($_POST['surname']) && !empty($_POST['surname']) && isset($_POST['sex']) && !empty($_POST['sex']) && isset($_POST['birthDate']) && !empty($_POST['birthDate']) && isset($_POST['pesel']) && !empty($_POST['pesel'])&& isset($_POST['street']) && !empty($_POST['street'])&& isset($_POST['houseNumber']) && !empty($_POST['houseNumber'])&& isset($_POST['city']) && !empty($_POST['city']) && isset($_POST['zipCode']) && !empty($_POST['zipCode']) && isset($_POST['startDate']) && !empty($_POST['startDate']) && isset($_POST['endDate']) && !empty($_POST['endDate']) && isset($_POST['crime']) && !empty($_POST['crime'])) {
 
         $prisonerId = $_POST['prisonerId'];
         $name = $_POST['name'];
         $surname = $_POST['surname'];
         $sex = $_POST['sex'];
         $birthDate = $_POST['birthDate'];
+        $pesel = $_POST['pesel'];
         $street = $_POST['street'];
         $houseNumber = $_POST['houseNumber'];
         $zipCode = $_POST['zipCode'];
@@ -22,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $dbconn = mysqli_connect("mysql.agh.edu.pl:3306", "anetabru", "Aneta30112001", "anetabru");
 
-        $query_prisoners = "UPDATE `prisoners` SET `name` = '$name', `surname`='$surname', `sex`='$sex', `birth_date`='$birthDate', `street`='$street', `house_number`='$houseNumber', `city`='$city', `zip_code`='$zipCode' WHERE `prisoner_id`='$prisonerId'";
+        $query_prisoners = "UPDATE `prisoners` SET `name` = '$name', `surname`='$surname', `sex`='$sex', `birth_date`='$birthDate', `street`='$street', `house_number`='$houseNumber', `city`='$city', `zip_code`='$zipCode', `pesel`='$pesel' WHERE `prisoner_id`='$prisonerId'";
 
         $result_prisoners = mysqli_query($dbconn, $query_prisoners);
 
